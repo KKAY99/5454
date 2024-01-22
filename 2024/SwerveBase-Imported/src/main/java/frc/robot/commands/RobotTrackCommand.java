f3a942306f2e865d46fcaffa79547aa18df0d793
NoBiscuit4U <razyback06@gmail.com>
2024-01-22 15:46:11 -0600

    Track Command to State Machine

--- 2024/SwerveBase-Imported/src/main/java/frc/robot/commands/RobotTrackCommand.java
+++ 2024/SwerveBase-Imported/src/main/java/frc/robot/commands/RobotTrackCommand.java
@@ -6,11 +6,17 @@ import frc.robot.subsystems.TurretSubsystem;
 import frc.robot.utilities.Limelight;
 import frc.robot.subsystems.TurretSubsystem;
 
-public class RobotTrackCommand extends Command {
+public class RobotTrackCommand extends Command{
   private Limelight m_limeLight;
   private TurretSubsystem m_turret;
   private boolean m_runLeft=true;
   private boolean m_runRight=false;
+
+  private enum TrackingStates{
+    TRACKING,SEARCHING
+  }
+
+  private TrackingStates m_state=TrackingStates.SEARCHING;
 
   public RobotTrackCommand(Limelight limeLight,TurretSubsystem turret){
     m_limeLight=limeLight;
@@ -24,43 +30,64 @@ public class RobotTrackCommand extends Command {
 
   @Override
   public boolean isFinished(){
-    if(m_limeLight.isTargetAvailible()){
-      double speed=0;
-      if(Math.abs(m_limeLight.getXRaw())<Constants.LimeLightValues.m_closeXCheck){
-        speed=Constants.LimeLightValues.limeLightTrackSpeed1;
-      }else if(Math.abs(m_limeLight.getXRaw())<Constants.LimeLightValues.m_medXCheck){
-        speed=Constants.LimeLightValues.limeLightTrackSpeed2;
-      }else if(Math.abs(m_limeLight.getXRaw())<Constants.LimeLightValues.m_farXCheck){
-        speed=Constants.LimeLightValues.limeLightTrackSpeed3;
+    double speed=0;
+
+    switch(m_state){
+    case TRACKING:
+      if(m_limeLight.isTargetAvailible()){
+        if(Math.abs(m_limeLight.getXRaw())<Constants.LimeLightValues.m_closeXCheck){
+          speed=Constants.LimeLightValues.limeLightTrackSpeed1;
+        }else if(Math.abs(m_limeLight.getXRaw())<Constants.LimeLightValues.m_medXCheck){
+          speed=Constants.LimeLightValues.limeLightTrackSpeed2;
+        }else if(Math.abs(m_limeLight.getXRaw())<Constants.LimeLightValues.m_farXCheck){
+          speed=Constants.LimeLightValues.limeLightTrackSpeed3;
+        }else{
+          speed=Constants.LimeLightValues.limeLightTrackSpeed4;
+        }
+
+        if(m_limeLight.getXRaw()<0){
+          m_turret.RunCheckLimits(speed);
+        }else if(m_limeLight.getXRaw()>0){
+          m_turret.RunCheckLimits(-speed);
+        }else{
+          m_turret.stop();
+          return true;
+        }
       }else{
-        speed=Constants.LimeLightValues.limeLightTrackSpeed4;
+        if(speed>0){
+          m_runRight=true;
+        }else if(speed<0){
+          m_runLeft=true;
+        }else{
+          m_runRight=true;
+        }
+        m_state=TrackingStates.SEARCHING;
       }
 
-    if(m_limeLight.getXRaw()<0){
-      m_turret.RunCheckLimits(speed);
-    }else if(m_limeLight.getXRaw()>0){
-      m_turret.RunCheckLimits(-speed);
-    }else{
-      m_turret.stop();
-      return true;
-    }
-    }else{
-      if(m_runLeft){
-        m_turret.RunCheckLimits(-Constants.LimeLightValues.limeLightTrackSpeed4);
-        if(m_turret.IsAtLeftLimit()){
-          m_runRight=true;
-          m_runLeft=false;
+    break;
+    case SEARCHING:
+      if(!m_limeLight.isTargetAvailible()){
+        if(m_runLeft){
+          m_turret.RunCheckLimits(-Constants.LimeLightValues.limeLightTrackSpeed4);
+          if(m_turret.IsAtLeftLimit()){
+            m_runRight=true;
+            m_runLeft=false;
+          }
         }
-      }
       
-      if(m_runRight){
-        m_turret.RunCheckLimits(Constants.LimeLightValues.limeLightTrackSpeed4);
-        if(m_turret.IsAtRightLimit()){
-          m_runRight=false;
-          m_runLeft=true;
+        if(m_runRight){
+          m_turret.RunCheckLimits(Constants.LimeLightValues.limeLightTrackSpeed4);
+          if(m_turret.IsAtRightLimit()){
+            m_runRight=false;
+            m_runLeft=true;
+          }
         }
+      }else{
+        m_state=TrackingStates.TRACKING;
       }
-    }
+    break;
     return false;
   }
+}
+
 }
