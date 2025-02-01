c090cbbe89e51107abef401b070dc2c57a8f4966
Karl Kay <kkay99@gmail.com>
2025-01-31 21:15:13 -0600

    motors

--- 2025/5454-2025-GameBot/src/main/java/frc/robot/subsystems/DunkinDonutSubsystem.java
+++ 2025/5454-2025-GameBot/src/main/java/frc/robot/subsystems/DunkinDonutSubsystem.java
@@ -4,18 +4,23 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import frc.robot.utilities.ObsidianCANSparkMax;
+
+import org.littletonrobotics.junction.Logger;
+
 import com.revrobotics.spark.*;
 
 public class DunkinDonutSubsystem extends SubsystemBase {
-  private ObsidianCANSparkMax m_motor1;
-  private ObsidianCANSparkMax m_motor2;
+  private ObsidianCANSparkMax m_coralMotor;
+  private ObsidianCANSparkMax m_algaeMotor;
   private ObsidianCANSparkMax m_rotateMotor;
-
+  private double m_rotateSpeed=0;
+  private double m_coralSpeed=0;
+  private double m_algaeSpeed=0;
   private SparkClosedLoopController m_loopController;
   
-  public DunkinDonutSubsystem(int CanID_1, int CanID_2, int rotateCanID) {
-    m_motor1 = new ObsidianCANSparkMax(CanID_1, MotorType.kBrushless, true);
-    m_motor2 = new ObsidianCANSparkMax(CanID_2, MotorType.kBrushless, true);
+  public DunkinDonutSubsystem(int coralCanID, int algaeCanID, int rotateCanID) {
+    m_coralMotor = new ObsidianCANSparkMax(coralCanID, MotorType.kBrushless, true);
+    m_algaeMotor= new ObsidianCANSparkMax(algaeCanID, MotorType.kBrushless, true);
     m_rotateMotor = new ObsidianCANSparkMax(rotateCanID, MotorType.kBrushless, true);
 
     m_loopController=m_rotateMotor.getClosedLoopController();
@@ -30,36 +35,45 @@ public class DunkinDonutSubsystem extends SubsystemBase {
     m_loopController.setReference(pos, ControlType.kPosition);
   }
 
-  public void reset_referamce(){
+  public void reset_referance(){
     m_loopController.setReference(0, ControlType.kVelocity);
   }
 
   public void run_rotatemotor(double speed){
+    m_rotateSpeed=speed;
     m_rotateMotor.set(speed);
   }
 
   public void stop_rotatemotor(){
+    m_rotateSpeed=0;
     m_rotateMotor.stopMotor();
   }
   
-  public void motor_runmotor(int motorNumber,double speed){
-    if(motorNumber==1){
-      m_motor1.set(speed);
-   } else {
-     m_motor2.set(speed);
-   }
+  public void runCoralMotor(double speed){
+    m_coralSpeed=speed;
+    m_coralMotor.set(speed); 
+  }
+  public void runAlgaeMotor(double speed){
+    m_algaeSpeed=speed;
+    m_algaeMotor.set(speed);
+  }
+  public void stopCoralMotor(double speed){
+    m_coralMotor.stopMotor();
+    m_coralSpeed=0;
+  }
+  public void stopAlgeaMotor(double speed){
+    m_algaeMotor.stopMotor();
+    m_algaeSpeed=0;
   }
 
-  public void motor_stop(int motorNumber){
-    if(motorNumber==1){
-      m_motor1.stopMotor();
-   } else {
-     m_motor2.stopMotor();
-   }
-  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  // This method will be called once per scheduler run
+    Logger.recordOutput("Dunkin/RotateSpeed", m_rotateSpeed);
+    Logger.recordOutput("Dunkin/CoralSpeed", m_coralSpeed);
+    Logger.recordOutput("Dunkin/AlgeaSpeed",m_algaeSpeed);
+    //Logger.recordOutput("Dunkinr/CurrentPosition",get_motor1pos());    
   }
 }
