9918a2d3cbe5bc10a259f809abaf1e9dabf9e3b7
Karl Kay <kkay99@gmail.com>
2025-02-14 20:49:32 -0600

    2/14/2025 elastic

--- 2025/5454-2025-GameBot/src/main/java/frc/robot/commands/ElevatorPosCommand.java
+++ 2025/5454-2025-GameBot/src/main/java/frc/robot/commands/ElevatorPosCommand.java
@@ -1,20 +1,30 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants.ElevatorConstants;
-import frc.robot.subsystems.ElevatorSubsystem;
-import frc.robot.Constants.ElevatorConstants.ElevatorScoreLevel;
 import java.util.function.Supplier;
 
 import com.revrobotics.spark.ClosedLoopSlot;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.ElevatorConstants;
+import frc.robot.Constants.ElevatorConstants.ElevatorScoreLevel;
+import frc.robot.Constants.NotificationLevel;
+import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.utilities.Elastic;
+
 public class ElevatorPosCommand extends Command {
+
+
   private ElevatorSubsystem m_elevator;
 
   private Supplier<ElevatorScoreLevel> m_scoreLevel;
   private ClosedLoopSlot m_closedLoopSlot;
 
   private double m_pos;
+
+  private String elevatorlevel;
+
+  Elastic.Notification notification = new Elastic.Notification(NotificationLevel.INFO, "Elevator level", elevatorlevel);
 
   public ElevatorPosCommand(ElevatorSubsystem elevator, double pos) {
     m_elevator=elevator;
@@ -42,26 +52,42 @@ public class ElevatorPosCommand extends Command {
       switch(m_scoreLevel.get()){
         case L1:
         m_pos=ElevatorConstants.l1Pos;
+        elevatorlevel = "Level 1";
         break;
         case L2:
         m_pos=ElevatorConstants.l2Pos;
+        elevatorlevel = "Level 2";
         break;
         case L3:
         m_pos=ElevatorConstants.l3Pos;
+        elevatorlevel = "Level 3";
         break;
         case L4:
         m_pos=ElevatorConstants.l4Pos;
+        elevatorlevel = "Level 4";
         break;
         case RETRACT:
+        elevatorlevel = "Retract";
         m_pos=ElevatorConstants.elevatorLowLimit;
-        break;
+        break;    
+     
+      
       }
+      notification.setDescription(elevatorlevel);
+
+      Elastic.sendNotification(notification);
+
+    }else{
+      elevatorlevel = "null";
     }
+
+    
   }  
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute(){
+    SmartDashboard.putString("Elevator Level", elevatorlevel);
   }
 
   // Called once the command ends or is interrupted.
