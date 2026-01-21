4760e6f6e4167eb0aab498bef5ddc52f096b688d
Karl Kay <kkay99@gmail.com>
2026-01-20 18:28:01 -0600

    Intake

---
+++ 2026/5454-2026-GameBot-Imported/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkAbsoluteEncoder;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utilities.ObsidianCANSparkMax;
+
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+
+public class IntakeSubsystem extends SubsystemBase {
+  private ObsidianCANSparkMax m_intakeMotor;
+  private ObsidianCANSparkMax m_lowMotor;
+  private DutyCycleEncoder m_encoder;
+  //private SparkAbsoluteEncoder m_encoder;
+
+  public IntakeSubsystem(int CanId1, int CanId2) {
+    m_intakeMotor = new ObsidianCANSparkMax(CanId1, MotorType.kBrushless, true);
+    m_lowMotor = new ObsidianCANSparkMax(CanId2, MotorType.kBrushless, true);
+  }
+
+  public void runIntake(double speed,double lowspeed) {
+    m_intakeMotor.set(speed);
+    m_lowMotor.set(lowspeed);
+  }
+
+  public void stopIntake(){
+    m_intakeMotor.stopMotor();
+    m_lowMotor.stopMotor();
+  }
+
+}
