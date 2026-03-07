cc5716e676d226788e5be2722b9287628ed2cced
Karl Kay <kkay99@gmail.com>
2026-03-07 13:06:26 -0600

    Fixed Intake Fold AutoSpeed

--- 2026/5454-2026-GameBot-Imported/src/main/java/frc/robot/Constants.java
+++ 2026/5454-2026-GameBot-Imported/src/main/java/frc/robot/Constants.java
@@ -147,7 +147,7 @@ public final class Constants {
     public static final double highSpeed=-1.0;
     public static final double outtakeSpeed=0.9;
     public static final double foldSpeed=-0.2;
-    public static final double foldSpeedAutoMode=0.15;
+    public static final double foldSpeedAutoMode=-0.15;
     public static final double ampStop=10;
   }
 
