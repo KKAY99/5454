9f3ae418e891168cd72514077cd5079e5801909f
asherbug <144501278+asherbug@users.noreply.github.com>
2026-03-14 18:55:40 -0500

    hood ratio fixed

--- 2026/5454-2026-GameBot-Imported/src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ 2026/5454-2026-GameBot-Imported/src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -8,11 +8,13 @@ import com.revrobotics.spark.SparkAbsoluteEncoder;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants;
 import frc.robot.Constants.ClimbConstants;
 import frc.robot.Constants.TurretConstants;
 import frc.robot.utilities.ObsidianCANSparkMax;
