c13a07c941fd5d39cb94d6ba44f02a945d4e5445
Jack0wack0 <75100830+Jack0wack0@users.noreply.github.com>
2026-03-18 16:02:32 -0500

    Configure drive motor current limits
    Added current limits configuration for drive motor. check.

--- 2026/5454-2026-GameBot-Imported/src/main/java/frc/robot/TunerConstants.java
+++ 2026/5454-2026-GameBot-Imported/src/main/java/frc/robot/TunerConstants.java
@@ -56,7 +56,14 @@ public class TunerConstants {
 
     // Initial configs for the drive and steer motors and the azimuth encoder; these cannot be null.
     // Some configs will be overwritten; check the `with*InitialConfigs()` API documentation.
-    private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration();
+    private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration()
+        .withCurrentLimits(
+            new CurrentLimitsConfigs()
+                .withStatorCurrentLimit(Amps.of(60))
+                .withSupplyCurrentLimit(Amps.of(50))
+                .withStatorCurrentLimitEnable(true)
+                .withSupplyCurrentLimitEnable(true)
+            );
     private static final TalonFXConfiguration steerInitialConfigs = new TalonFXConfiguration()
         .withCurrentLimits(
             new CurrentLimitsConfigs()
