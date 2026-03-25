259abf190001bbfa0c03a00ee31ae55c22d95dd6
Karl Kay <kkay99@gmail.com>
2026-03-24 21:25:24 -0500

    updated kP

--- 2026/5454-2026-GameBot-Imported/src/main/java/frc/robot/TunerConstants.java
+++ 2026/5454-2026-GameBot-Imported/src/main/java/frc/robot/TunerConstants.java
@@ -25,7 +25,7 @@ public class TunerConstants {
     // The steer motor uses any SwerveModule.SteerRequestType control request with the
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains = new Slot0Configs()
-        .withKP(100).withKI(0).withKD(0.5)
+        .withKP(25).withKI(0).withKD(0.5)
         .withKS(0.1).withKV(2.05).withKA(0)
         .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
     // When using closed-loop control, the drive motor uses the control
